000c16aa6f02e8a1c8d1f0676fcee0910d635285
Twhite2 <emmanuelmieye@gmail.com>
2023-04-17 10:03:33 +0100

    mern chat front end and back end

---
+++ mern-chat-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
