cc960e8cf4d0c6f892585a8485f6785b0d7e99fb
Twhite2 <emmanuelmieye@gmail.com>
2023-04-18 10:08:47 +0100

    frontend update, backend server

--- mern-chat-frontend/src/pages/Home.js
+++ mern-chat-frontend/src/pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
 
 function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    return (
+        <Row>
+            <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+                <div>
+                    <h1>Hi, nice to meet yoy. Kimi no nawa?</h1>
+                    <p>Here is where strangers become less strange</p>
+                    <LinkContainer to="/chat">
+                        <Button variant="success">
+                            Get Started <i className="fas fa-comments home-message-icon"></i>
+                        </Button>
+                    </LinkContainer>
+                </div>
+            </Col>
+            <Col md={6} className="home__bg"></Col>
+        </Row>
+    );
 }
 
-export default Home
+export default Home;
